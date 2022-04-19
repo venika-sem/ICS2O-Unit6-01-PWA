5f6e10fbd303591491ef71f31336a1e8f60c8db4
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-04-19 13:12:31 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* Created by: Venika Sem
+ * Created on: Apr 2022
+ * This file contains the JS functions for index.html 
+*/
+
+'use strict'
+/**
+ * This function converts the temperature from fahrenheit to celsius.
+ */
+function convert() {
+  // input
+  const fahrenheitTemperature = parseFloat(document.getElementById('fahrenheit-temperature').value)
+
+   // process
+  const temperature = (fahrenheitTemperature - 32) * 5 / 9
+
+  // output
+  document.getElementById('temperature').innerHTML = '<p>The temperature in Celsius is: ' + temperature.toFixed(2) + ' °C</p>'
+}
